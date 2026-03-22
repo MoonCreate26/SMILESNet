b34f12004c0cc32cd70ed99f5dd1f53e67fea0ba
Moon Create <mricky5555@gmail.com>
2026-03-22 17:00:12 -0500

    fingerprint -> NParr

--- CompoundAnalysis.py
+++ CompoundAnalysis.py
@@ -1,13 +1,20 @@
-import numpy as np;
+# Main imports
 import kagglehub
+import numpy as np;
 import pandas as pd
+# Scikit-learn
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+# RDKit
 from rdkit import Chem
-from rdkit.Chem import rdFingerprintGenerator
+from rdkit.Chem import rdFingerprintGenerator, DataStructs
+
 
 # Load the dataset
 df = pd.read_csv('SMILES_Big_Data_Set.csv')
 
-# Convert SMILES -> mol -> fingerprints
+# Convert SMILES -> mol -> Bit Vector -> Np Array
 fp_gen = rdFingerprintGenerator.GetMorganGenerator(radius=2, fpSize=2048)
 
 def get_fp(mol):
@@ -15,9 +22,16 @@ def get_fp(mol):
         return fp_gen.GetFingerprint(mol)
     return None
 
-df['fingerprint'] = df['SMILES'].apply(lambda x: Chem.MolFromSmiles(x)).apply(get_fp)
+def fp_to_array(fp):
+    arr = np.zeros((0,), dtype=np.int8)
+    DataStructs.ConvertToNumpyArray(fp, arr)
+    return arr
 
+df['fingerprint'] = df['SMILES'].apply(lambda x: Chem.MolFromSmiles(x)).apply(get_fp).apply(lambda x: fp_to_array(x) if x else None)
+
+# Set input and output
 x = df['fingerprint']
+y = df['logP']
 
-
-print(x.iloc[0])
+print(f"Input: Fingerprint\n{x}\n")
+print(f"Output: logP\n{y}")
