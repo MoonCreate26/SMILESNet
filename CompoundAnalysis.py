b0ca9a6557c5cc48bafb64543ba2cb939af2ae69
Moon Create <mricky5555@gmail.com>
2026-03-22 16:26:31 -0500

    Convert SMILES to Vector

--- CompoundAnalysis.py
+++ CompoundAnalysis.py
@@ -1,13 +1,23 @@
 import numpy as np;
 import kagglehub
-
 import pandas as pd
-import os
-
-# List files to confirm path
-print(os.listdir('.'))
+from rdkit import Chem
+from rdkit.Chem import rdFingerprintGenerator
 
 # Load the dataset
-df = pd.read_csv('your-file-name.csv')
+df = pd.read_csv('SMILES_Big_Data_Set.csv')
 
-print(df.head())
+# Convert SMILES -> mol -> fingerprints
+fp_gen = rdFingerprintGenerator.GetMorganGenerator(radius=2, fpSize=2048)
+
+def get_fp(mol):
+    if mol:
+        return fp_gen.GetFingerprint(mol)
+    return None
+
+df['fingerprint'] = df['SMILES'].apply(lambda x: Chem.MolFromSmiles(x)).apply(get_fp)
+
+x = df['fingerprint']
+
+
+print(x.iloc[0])
